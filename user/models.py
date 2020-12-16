23833c841fca9c46b51824179b5a9875c946dd42
adilsonoj <adilsonoj@yahoo.com.br>
2020-12-16 15:56:17 -0300

    adiciona model Role

--- user/models.py
+++ user/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=30)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
 class User(models.Model):
     name = models.CharField(max_length=100)
+    role = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
     created_at = models.DateField()
 
     def __str__(self):
